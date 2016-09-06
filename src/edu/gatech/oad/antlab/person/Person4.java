d13c48815b9bd04e5b09883d792aa869d0be751e
Alex O'Connell <oconnella19@gatech.edu>
2016-09-06 12:45:53 -0400

    Add my implementation of Person4.java

--- src/edu/gatech/oad/antlab/person/Person4.java
+++ src/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String ret = "";
+      for(int i = 0; i < input.length(); i++) {
+    	char c = input.charAt(i);
+    	c++;
+    	ret += c;
+      }
+      return ret;
     }
     
     /**
