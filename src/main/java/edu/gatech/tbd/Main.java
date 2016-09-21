736aef1cfae7165af7a1792c95106231ba94c409
Scott Wolfe <swolfe8@gatech.edu>
2016-09-21 14:08:40 -0400

    Edited/Created controller for LoginStage and ApplicationStage. Added Application Stage. This should complete M4's coding.

--- src/main/java/edu/gatech/tbd/Main.java
+++ src/main/java/edu/gatech/tbd/Main.java
@@ -2,15 +2,18 @@ package edu.gatech.tbd;
 
 import java.io.IOException;
 import java.net.URL;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import edu.gatech.tbd.controllers.*;
 
 public class Main extends Application {
 	
+    /**
+     * main stage for application
+     */
 	private static Stage stage;
 
 	@Override
@@ -18,12 +21,21 @@ public class Main extends Application {
 		Parent root;
 		stage = primaryStage;
 		try {
-			root = FXMLLoader.load(getClass().getResource("/edu/gatech/tbd/stages/LoginStage.fxml"));
-		    Scene scene = new Scene(root);
+		    // loading login stage
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource(
+			        "/edu/gatech/tbd/stages/LoginStage.fxml"));
+			root = loader.load();
+			Scene scene = new Scene(root);
+		    
+		    // adding Main reference to LoginStageController
+		    LoginStageController controller = loader.getController();
+		    controller.setMainApp(this);
 		    
 	        stage.setTitle("Team 19");
 	        stage.setScene(scene);
 	        stage.show();
+	        
 		} catch (IOException e) {
 			System.err.println("Error loading main stage!");
 			e.printStackTrace();
@@ -33,4 +45,54 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	/**
+     * Changes scene to LoginStage
+     */
+    public void showLoginStage() { 
+        try {
+            // loading LoginStage
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(
+                    "/edu/gatech/tbd/stages/LoginStage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            
+            // adding Main reference to LoginStageController
+            LoginStageController controller = loader.getController();
+            controller.setMainApp(this);
+            
+            stage.setScene(scene);
+            stage.show();
+            
+        } catch (IOException e) {
+            System.err.println("Error loading login stage!");
+            e.printStackTrace();
+        }
+    }
+	
+	/**
+	 * Changes scene to ApplicationStage
+	 */
+	public void showApplicationStage() { 
+	    try {
+	        // loading ApplicationStage
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Main.class.getResource("/edu/gatech/tbd/stages/ApplicationStage.fxml"));
+	        Parent root = loader.load();
+            Scene scene = new Scene(root);
+            
+            // adding Main reference to ApplicationStageController
+            ApplicationStageController controller = loader.getController();
+            controller.setMainApp(this);
+            
+            stage.setScene(scene);
+	        stage.show();
+	        
+	    } catch (IOException e) {
+	        System.err.println("Error loading application stage!");
+            e.printStackTrace();
+	    }
+	}
+	
 }
--- src/main/java/edu/gatech/tbd/controllers/LoginStageController.java
+++ src/main/java/edu/gatech/tbd/controllers/LoginStageController.java
@@ -6,9 +6,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import edu.gatech.tbd.Main;
 
 public class LoginStageController {
 	
+    /**
+     * Reference to main application class
+     */
+    Main mainApp;
+    
 	@FXML
 	Label errorLabel;
 	
@@ -25,7 +31,27 @@ public class LoginStageController {
 	
 	@FXML
 	protected void onLoginButtonPressed(ActionEvent e) {
-		 
+        
+	    // Getting username/password data from textfields
+	    String username = usernameField.getText();
+	    String password = passwordField.getText(); 
+	    
+	    // if username/password combo matches stored data
+	    if (username.equals("user") && password.equals("pass")) {
+	        mainApp.showApplicationStage();
+	    }
+	    // if username/password combo does not match stored data
+	    else {
+	        errorLabel.setText(
+	                "Login Failed: Invalid Username or Password");
+	        usernameField.setText("");
+	        passwordField.setText("");
+	    }
+	}
+	
+	/**setter for mainApp*/
+	public void setMainApp(Main mainApp) {
+	    this.mainApp = mainApp;
 	}
 
 }
