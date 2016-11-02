33144b15a6316da8f01a23fdc7e7ffd068287c36
Chase <dmtninja@gmail.com>
2016-11-02 13:39:23 -0400

    made code more efficient

--- src/main/java/edu/gatech/tbd/controller/ApplicationSceneController.java
+++ src/main/java/edu/gatech/tbd/controller/ApplicationSceneController.java
@@ -407,285 +407,37 @@ public class ApplicationSceneController extends SceneController {
 			}
 		}
 
-		List<PurityReport> janPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> febPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> marchPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> aprilPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> mayPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> junePurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> julyPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> augPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> sepPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> octPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> novPurityReports = new ArrayList<PurityReport>();
-		List<PurityReport> decPurityReports = new ArrayList<PurityReport>();
+		int[] virusSumsByMonth = new int[12];
+		int[] contamSumsByMonth = new int[12];
+		int[] virusCountByMonth = new int[12];
+		int[] contamCountByMonth = new int[12];
 
-		for (int i = 0; i < yearlyPurityReportList.size(); i++) {
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '1') {
-				janPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '2') {
-				febPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '3') {
-				marchPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '4') {
-				aprilPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '4') {
-				aprilPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '5') {
-				mayPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '6') {
-				junePurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '7') {
-				julyPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '8') {
-				augPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '0' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '9') {
-				sepPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '1' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '0') {
-				octPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '1' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '1') {
-				novPurityReports.add(yearlyPurityReportList.get(i));
-			}
-			if (yearlyPurityReportList.get(i).getDateTime().charAt(5) == '1' && yearlyPurityReportList.get(i).getDateTime().charAt(6) == '2') {
-				decPurityReports.add(yearlyPurityReportList.get(i));
-			}
+		for (PurityReport report: yearlyPurityReportList) {
+			String month = (String.valueOf(report.getDateTime().charAt(5)) + String.valueOf(report.getDateTime().charAt(6)));
+			System.out.println(month);
+			int monthInt = Integer.parseInt(month);
+			virusSumsByMonth[monthInt-1] += report.getVirusPPM();
+			virusCountByMonth[monthInt-1]++;
+			contamSumsByMonth[monthInt-1] += report.getContaminantPPM();
+			contamCountByMonth[monthInt-1]++;
+
 		}
 
-		/*
-		int[] yearlyVirusPPM = new int[12];
-		int[] yearlyContaminantPPM = new int[12];
+		int[] virusAveragesByMonth = new int[12];
+		int[] contamAveragesByMonth = new int[12];
 
-		for (int i = 0; i < yearlyPurityReportList.size(); i++) {
-			yearlyPurityReportList.get(i).getMonth() - 1
+		for (int i = 0; i < 12; i++) {
+			if (virusCountByMonth[i] == 0) {
+				virusAveragesByMonth[i] = 0;
+			} else {
+				virusAveragesByMonth[i] = virusSumsByMonth[i]/virusCountByMonth[i];
+			}
+			if (contamCountByMonth[i] == 0) {
+				contamAveragesByMonth[i] = 0;
+			} else {
+				contamAveragesByMonth[i] = contamSumsByMonth[i]/contamCountByMonth[i];
+			}
 		}
-		*/
-
-        int avgJanVirusPPM;
-        int avgJanContaminantPPM;
-        int janVirusSum = 0;
-        int janContamSum = 0;
-
-        if (janPurityReports.size() == 0) {
-        	avgJanVirusPPM = 0;
-        	avgJanContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < janPurityReports.size(); i++) {
-        		janVirusSum = janVirusSum + janPurityReports.get(i).getVirusPPM();
-        		janContamSum = janContamSum + janPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgJanVirusPPM = janVirusSum / janPurityReports.size();
-        	avgJanContaminantPPM = janContamSum / janPurityReports.size();
-        }
-
-        int avgFebVirusPPM;
-        int avgFebContaminantPPM;
-        int febVirusSum = 0;
-        int febContamSum = 0;
-
-        if (febPurityReports.size() == 0) {
-        	avgFebVirusPPM = 0;
-        	avgFebContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < febPurityReports.size(); i++) {
-        		febVirusSum = febVirusSum + febPurityReports.get(i).getVirusPPM();
-        		febContamSum = febContamSum + febPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgFebVirusPPM = febVirusSum / febPurityReports.size();
-        	avgFebContaminantPPM = febContamSum / febPurityReports.size();
-        }
-
-        int avgMarchVirusPPM;
-        int avgMarchContaminantPPM;
-        int marchVirusSum = 0;
-        int marchContamSum = 0;
-
-        if (marchPurityReports.size() == 0) {
-        	avgMarchVirusPPM = 0;
-        	avgMarchContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < marchPurityReports.size(); i++) {
-        		marchVirusSum = marchVirusSum + marchPurityReports.get(i).getVirusPPM();
-        		marchContamSum = marchContamSum + marchPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgMarchVirusPPM = marchVirusSum / marchPurityReports.size();
-        	avgMarchContaminantPPM = marchContamSum / marchPurityReports.size();
-        }
-
-        int avgAprilVirusPPM;
-        int avgAprilContaminantPPM;
-        int aprilVirusSum = 0;
-        int aprilContamSum = 0;
-
-        if (aprilPurityReports.size() == 0) {
-        	avgAprilVirusPPM = 0;
-        	avgAprilContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < aprilPurityReports.size(); i++) {
-        		aprilVirusSum = aprilVirusSum + aprilPurityReports.get(i).getVirusPPM();
-        		aprilContamSum = aprilContamSum + aprilPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgAprilVirusPPM = aprilVirusSum / aprilPurityReports.size();
-        	avgAprilContaminantPPM = aprilContamSum / aprilPurityReports.size();
-        }
-
-        int avgMayVirusPPM;
-        int avgMayContaminantPPM;
-        int mayVirusSum = 0;
-        int mayContamSum = 0;
-
-        if (mayPurityReports.size() == 0) {
-        	avgMayVirusPPM = 0;
-        	avgMayContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < mayPurityReports.size(); i++) {
-        		mayVirusSum = mayVirusSum + mayPurityReports.get(i).getVirusPPM();
-        		mayContamSum = mayContamSum + mayPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgMayVirusPPM = mayVirusSum / mayPurityReports.size();
-        	avgMayContaminantPPM = mayContamSum / mayPurityReports.size();
-        }
-
-        int avgJuneVirusPPM;
-        int avgJuneContaminantPPM;
-        int juneVirusSum = 0;
-        int juneContamSum = 0;
-
-        if (junePurityReports.size() == 0) {
-        	avgJuneVirusPPM = 0;
-        	avgJuneContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < junePurityReports.size(); i++) {
-        		juneVirusSum = juneVirusSum + junePurityReports.get(i).getVirusPPM();
-        		juneContamSum = juneContamSum + junePurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgJuneVirusPPM = juneVirusSum / junePurityReports.size();
-        	avgJuneContaminantPPM = juneContamSum / junePurityReports.size();
-        }
-
-        int avgJulyVirusPPM;
-        int avgJulyContaminantPPM;
-        int julyVirusSum = 0;
-        int julyContamSum = 0;
-
-        if (julyPurityReports.size() == 0) {
-        	avgJulyVirusPPM = 0;
-        	avgJulyContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < julyPurityReports.size(); i++) {
-        		julyVirusSum = julyVirusSum + julyPurityReports.get(i).getVirusPPM();
-        		julyContamSum = julyContamSum + julyPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgJulyVirusPPM = julyVirusSum / julyPurityReports.size();
-        	avgJulyContaminantPPM = julyContamSum / julyPurityReports.size();
-        }
-
-        int avgAugustVirusPPM;
-        int avgAugustContaminantPPM;
-        int augVirusSum = 0;
-        int augContamSum = 0;
-
-        if (augPurityReports.size() == 0) {
-        	avgAugustVirusPPM = 0;
-        	avgAugustContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < augPurityReports.size(); i++) {
-        		augVirusSum = augVirusSum + augPurityReports.get(i).getVirusPPM();
-        		augContamSum = augContamSum + augPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgAugustVirusPPM = augVirusSum / augPurityReports.size();
-        	avgAugustContaminantPPM = augContamSum / augPurityReports.size();
-        }
-
-        int avgSeptemberVirusPPM;
-        int avgSeptemberContaminantPPM;
-        int sepVirusSum = 0;
-        int sepContamSum = 0;
-
-        if (sepPurityReports.size() == 0) {
-        	avgSeptemberVirusPPM = 0;
-        	avgSeptemberContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < sepPurityReports.size(); i++) {
-        		sepVirusSum = sepVirusSum + sepPurityReports.get(i).getVirusPPM();
-        		sepContamSum = sepContamSum + sepPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgSeptemberVirusPPM = sepVirusSum / sepPurityReports.size();
-        	avgSeptemberContaminantPPM = sepContamSum / sepPurityReports.size();
-        }
-
-        int avgOctoberVirusPPM;
-        int avgOctoberContaminantPPM;
-        int octVirusSum = 0;
-        int octContamSum = 0;
-
-        if (octPurityReports.size() == 0) {
-        	avgOctoberVirusPPM = 0;
-        	avgOctoberContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < octPurityReports.size(); i++) {
-        		octVirusSum = octVirusSum + octPurityReports.get(i).getVirusPPM();
-        		octContamSum = octContamSum + octPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgOctoberVirusPPM = octVirusSum / octPurityReports.size();
-        	avgOctoberContaminantPPM = octContamSum / octPurityReports.size();
-        }
-
-        int avgNovemberVirusPPM;
-        int avgNovemberContaminantPPM;
-        int novVirusSum = 0;
-        int novContamSum =0;
-
-        if (novPurityReports.size() == 0) {
-        	avgNovemberVirusPPM = 0;
-        	avgNovemberContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < novPurityReports.size(); i++) {
-        		novVirusSum = novVirusSum + novPurityReports.get(i).getVirusPPM();
-        		novContamSum = novContamSum + novPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgNovemberVirusPPM = novVirusSum / novPurityReports.size();
-        	avgNovemberContaminantPPM = novContamSum / novPurityReports.size();
-        }
-
-        int avgDecemberVirusPPM;
-        int avgDecemberContaminantPPM;
-        int decVirusSum = 0;
-        int decContamSum = 0;
-
-        if ( decPurityReports.size() == 0) {
-        	avgDecemberVirusPPM = 0;
-        	avgDecemberContaminantPPM = 0;
-        } else {
-        	for (int i = 0; i < decPurityReports.size(); i++) {
-        		decVirusSum = decVirusSum + decPurityReports.get(i).getVirusPPM();
-        		decContamSum = decContamSum + decPurityReports.get(i).getContaminantPPM();
-        	}
-
-        	avgDecemberVirusPPM = decVirusSum / decPurityReports.size();
-        	avgDecemberContaminantPPM = decContamSum / decPurityReports.size();
-        }
 
         historicalReportGraph.setTitle("Historical Purity Graph");
 
@@ -694,36 +446,36 @@ public class ApplicationSceneController extends SceneController {
         LineChart.Series<String, Number> series = new LineChart.Series<String, Number>();
         series.setName("Virus PPM");
 
-        series.getData().add(new XYChart.Data<String, Number>("Jan", avgJanVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Feb", avgFebVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Mar", avgMarchVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Apr", avgAprilVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("May", avgMayVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Jun", avgJuneVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Jul", avgJulyVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Aug", avgAugustVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Sep", avgSeptemberVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Oct", avgOctoberVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Nov", avgNovemberVirusPPM));
-        series.getData().add(new XYChart.Data<String, Number>("Dec", avgDecemberVirusPPM));
+        series.getData().add(new XYChart.Data<String, Number>("Jan", virusAveragesByMonth[0]));
+        series.getData().add(new XYChart.Data<String, Number>("Feb", virusAveragesByMonth[1]));
+        series.getData().add(new XYChart.Data<String, Number>("Mar", virusAveragesByMonth[2]));
+        series.getData().add(new XYChart.Data<String, Number>("Apr", virusAveragesByMonth[3]));
+        series.getData().add(new XYChart.Data<String, Number>("May", virusAveragesByMonth[4]));
+        series.getData().add(new XYChart.Data<String, Number>("Jun", virusAveragesByMonth[5]));
+        series.getData().add(new XYChart.Data<String, Number>("Jul", virusAveragesByMonth[6]));
+        series.getData().add(new XYChart.Data<String, Number>("Aug", virusAveragesByMonth[7]));
+        series.getData().add(new XYChart.Data<String, Number>("Sep", virusAveragesByMonth[8]));
+        series.getData().add(new XYChart.Data<String, Number>("Oct", virusAveragesByMonth[9]));
+        series.getData().add(new XYChart.Data<String, Number>("Nov", virusAveragesByMonth[10]));
+        series.getData().add(new XYChart.Data<String, Number>("Dec", virusAveragesByMonth[11]));
 
         lineChartData.add(series);
 
         LineChart.Series<String, Number> series1 = new LineChart.Series<String, Number>();
         series1.setName("Contaminent PPM");
 
-        series1.getData().add(new XYChart.Data<String, Number>("Jan", avgJanContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Feb", avgFebContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Mar", avgMarchContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Apr", avgAprilContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("May", avgMayContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Jun", avgJuneContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Jul", avgJulyContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Aug", avgAugustContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Sep", avgSeptemberContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Oct", avgOctoberContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Nov", avgNovemberContaminantPPM));
-        series1.getData().add(new XYChart.Data<String, Number>("Dec", avgDecemberContaminantPPM));
+        series1.getData().add(new XYChart.Data<String, Number>("Jan", contamAveragesByMonth[0]));
+        series1.getData().add(new XYChart.Data<String, Number>("Feb", contamAveragesByMonth[1]));
+        series1.getData().add(new XYChart.Data<String, Number>("Mar", contamAveragesByMonth[2]));
+        series1.getData().add(new XYChart.Data<String, Number>("Apr", contamAveragesByMonth[3]));
+        series1.getData().add(new XYChart.Data<String, Number>("May", contamAveragesByMonth[4]));
+        series1.getData().add(new XYChart.Data<String, Number>("Jun", contamAveragesByMonth[5]));
+        series1.getData().add(new XYChart.Data<String, Number>("Jul", contamAveragesByMonth[6]));
+        series1.getData().add(new XYChart.Data<String, Number>("Aug", contamAveragesByMonth[7]));
+        series1.getData().add(new XYChart.Data<String, Number>("Sep", contamAveragesByMonth[8]));
+        series1.getData().add(new XYChart.Data<String, Number>("Oct", contamAveragesByMonth[9]));
+        series1.getData().add(new XYChart.Data<String, Number>("Nov", contamAveragesByMonth[10]));
+        series1.getData().add(new XYChart.Data<String, Number>("Dec", contamAveragesByMonth[11]));
 
         lineChartData.add(series1);
 
